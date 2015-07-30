90329ac6c3ba07947dc755877aa6adda5b41d674
Adam Ryman <aryman@splunk.com>
2015-07-29 21:39:38 -0700

    Update templates; Add stub code

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ var output = swig.renderFile('./opensearch-template/chrome-splunk.xml', {
 });
 
 console.log(output);
+
+
+// TODO: Loop through all browsers and render open search provider xml
+var testRendering = function () {
+
+}
+
+// TODO: Render, using browser to find template xml file
+var renderBrowser = function (host, port, https, browser) {
+
+}
